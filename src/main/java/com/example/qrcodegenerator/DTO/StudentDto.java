fe372f082c404661229321a095b26f0b1f678c12
decagon <samuel.okafor@decagon.dev>
2023-08-30 18:51:04 +0100

    update

---
+++ src/main/java/com/example/qrcodegenerator/DTO/StudentDto.java
@@ -0,0 +1,17 @@
+package com.example.qrcodegenerator.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.NaturalId;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDto {
+    private String firstName;
+    private String lastName;
+    @NaturalId(mutable = true)
+    private String email;
+    private Integer mobileNumber;
+}
