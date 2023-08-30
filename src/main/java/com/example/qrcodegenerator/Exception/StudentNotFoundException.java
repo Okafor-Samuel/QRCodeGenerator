43a00a416597683c966a73fcf698ded416847c92
decagon <samuel.okafor@decagon.dev>
2023-08-30 19:07:23 +0100

    update

---
+++ src/main/java/com/example/qrcodegenerator/Exception/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.qrcodegenerator.Exception;
+
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message){
+        super(message);
+    }
+}
