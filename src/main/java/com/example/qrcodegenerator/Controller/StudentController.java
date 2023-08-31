242081ebef53244dc17c1a9c87dd377a3137bb50
decagon <samuel.okafor@decagon.dev>
2023-08-31 02:06:12 +0100

    update

--- src/main/java/com/example/qrcodegenerator/Controller/StudentController.java
+++ src/main/java/com/example/qrcodegenerator/Controller/StudentController.java
@@ -3,10 +3,13 @@ package com.example.qrcodegenerator.Controller;
 import com.example.qrcodegenerator.DTO.StudentDto;
 import com.example.qrcodegenerator.Model.Student;
 import com.example.qrcodegenerator.Service.StudentService;
+import com.example.qrcodegenerator.Utils.QRCodeGenerator;
+import com.google.zxing.WriterException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -16,7 +19,13 @@ public class StudentController {
     private final StudentService studentService;
 
     @GetMapping("/get-all-student")
-    private ResponseEntity<List<Student>> getStudents(){
+    private ResponseEntity<List<Student>> getStudents() throws IOException, WriterException {
+        List<Student> students = studentService.getStudents().getBody();
+        if(students.size() !=0){
+            for (Student student: students){
+                QRCodeGenerator.generateQRCode(new StudentDto());
+            }
+        }
         return studentService.getStudents();}
 
     @PostMapping("/add-student")
