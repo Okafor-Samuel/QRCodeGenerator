7413c2899b50e72996f806a1a6252c84da53dbb0
decagon <samuel.okafor@decagon.dev>
2023-08-31 01:59:32 +0100

    update

--- src/main/java/com/example/qrcodegenerator/Utils/QRCodeGenerator.java
+++ src/main/java/com/example/qrcodegenerator/Utils/QRCodeGenerator.java
@@ -3,11 +3,16 @@ package com.example.qrcodegenerator.Utils;
 import com.example.qrcodegenerator.DTO.StudentDto;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class QRCodeGenerator {
-    public static void generateQRCode(StudentDto studentDto) throws WriterException {
+    public static void generateQRCode(StudentDto studentDto) throws WriterException, IOException {
 
         String qrCodePath ="/Users/decagon/Desktop/QRCodeFolder";
         String qrCodeName = qrCodePath+studentDto.getEmail()+ "-QRCODE.png";
@@ -17,5 +22,7 @@ public class QRCodeGenerator {
                         "Last Name: "+studentDto.getLastName()+"\n"+
                         "Email: "+studentDto.getEmail()+"\n"+
                         "Mobile Number: "+studentDto.getMobileNumber(), BarcodeFormat.QR_CODE,400,400);
+        Path path = FileSystems.getDefault().getPath(qrCodeName);
+        MatrixToImageWriter.writeToPath(bitMatrix,"PNG",path);
     }
 }
